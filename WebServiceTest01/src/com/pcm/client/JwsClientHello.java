0ba9b2ad8969846cc7748739720a94f8d51778db
sipanwang <sipanwang@163.com>
2018-06-21 16:33:19 +0800

    Java WebServcie接口的生成和调用01

---
+++ WebServiceTest01/src/com/pcm/client/JwsClientHello.java
@@ -0,0 +1,20 @@
+package com.pcm.client;
+import com.pcm.ws.jms.JwsServiceHello;
+import com.pcm.ws.jms.JwsServiceHelloService;
+
+/**
+ * 
+* Title: JwsClientHello
+* Description: webService 客户端调用
+* Version:1.0.0  
+* @author panchengming
+ */
+public class JwsClientHello {
+
+    public static void main(String[] args) {
+         //调用webservice
+        JwsServiceHello hello=new JwsServiceHelloService().getJwsServiceHelloPort();
+        String name=hello.getValue("贝多芬");
+        System.out.println(name);
+    }
+}
